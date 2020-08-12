47085f41fab1610a344f940c951e66ef47e3678d
Stacy Read <sread@wctc.edu>
2020-08-12 13:14:17 -0500

    Initial commit

---
+++ src/main/java/edu/wctc/pmnh/HomeController.java
@@ -0,0 +1,22 @@
+package edu.wctc.pmnh;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/forward-me")
+    public String doForward() {
+        return "forward:/fossils/facts";
+    }
+
+    @RequestMapping("/redirect-me")
+    public String doRedirect() {
+        return "redirect:/glaciers/trail";
+    }
+
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+}
