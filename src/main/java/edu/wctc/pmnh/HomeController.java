6cae1303443c0f6e1017befcfe5ed99a2a8f7fd5
tayjay1019 <tjoseph1@my.wctc.edu>
2021-10-07 19:07:06 -0500

    initial commit

--- src/main/java/edu/wctc/pmnh/HomeController.java
+++ src/main/java/edu/wctc/pmnh/HomeController.java
@@ -5,16 +5,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/forward-me")
-    public String doForward() {
-        return "forward:/fossils/facts";
+    //@RequestMapping("/forward-me")
+    //public String doForward() {
+        //return "forward : ";
+    //}
+
+    @RequestMapping("/step1")
+    public String showStep1() {
+        return "pages/step1";
+    }
+    @RequestMapping("/step2")
+    public String showStep2() {
+        return "pages/step2";
+    }
+    @RequestMapping("/step3")
+    public String showStep3() {
+        return "pages/step3";
     }
 
-    @RequestMapping("/redirect-me")
-    public String doRedirect() {
-        return "redirect:http://www.wctc.edu";
+    //@RequestMapping("/redirect-me")
+    //public String doRedirect() {
+        //return "redirect:http://www.wctc.edu";
 //        return "redirect:/glaciers/trail";
-    }
+   // }
 
     @RequestMapping("/")
     public String showHomePage() {
